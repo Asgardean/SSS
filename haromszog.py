19546d101d5e10eb4f8c78ffaaf615bfaaff1fa0
Asgardean <varnaiviktor0@gmail.com>
2023-05-04 14:08:05 +0200

    Kész

--- haromszog.py
+++ haromszog.py
@@ -2,3 +2,9 @@
 a = int(input("Add meg a háromszög A oldalát"))
 b = int(input("Add meg a háromszög B oldalát"))
 c = int(input("Add meg a háromszög C oldalát"))
+if c < a+b and a < b+c and b<a+c:
+	print('A(z)', a, ',', b, 'és', c, 'háromszöget alkot')
+else:
+	print('A(z)', a, ',', b, 'és', c, 'nem alkot háromszöget')
+
+
